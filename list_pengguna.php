1f99263176ad859726a7c6df7ead76291a1a37b4
Han2Soon <raihanmustafa2002@gmail.com>
2023-06-13 12:41:29 -0700

    add list_pengguna.php

--- list_pengguna.php
+++ list_pengguna.php
@@ -86,6 +86,25 @@
         </tbody>
     </table>
 </div>
+
+<script>
+  // Hide success alert after 2 seconds
+  setTimeout(function() {
+    var successAlert = document.getElementById('success-alert');
+    if (successAlert) {
+      successAlert.style.display = 'none';
+    }
+  }, 2000);
+
+  // Hide error alert after 2 seconds
+  setTimeout(function() {
+    var errorAlert = document.getElementById('error-alert');
+    if (errorAlert) {
+      errorAlert.style.display = 'none';
+    }
+  }, 2000);
+</script>
+
 <?php include 'footer.php' ?>
 </body>
 </html>
